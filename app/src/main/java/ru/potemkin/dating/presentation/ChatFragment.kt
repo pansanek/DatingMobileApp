aa4fd0a1d9217d77d39c2da1ce33d00027b1f7f7
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-10 15:05:15 +0300

    Создание файлов в presentation слое и макеты

---
+++ app/src/main/java/ru/potemkin/dating/presentation/ChatFragment.kt
@@ -0,0 +1,6 @@
+package ru.potemkin.dating.presentation
+
+import androidx.fragment.app.Fragment
+
+class ChatFragment : Fragment() {
+}
---
+++ app/src/main/java/ru/potemkin/dating/presentation/ChatListFragment.kt
@@ -0,0 +1,6 @@
+package ru.potemkin.dating.presentation
+
+import androidx.fragment.app.Fragment
+
+class ChatListFragment : Fragment() {
+}
