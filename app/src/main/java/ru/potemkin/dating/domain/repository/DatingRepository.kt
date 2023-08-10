b67572c77b050756d7b9f4f20b7a4935a9849ad1
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-10 14:48:08 +0300

    Исправление возвращаемого типа в репозитории

--- app/src/main/java/ru/potemkin/dating/domain/repository/DatingRepository.kt
+++ app/src/main/java/ru/potemkin/dating/domain/repository/DatingRepository.kt
@@ -1,55 +1,56 @@
 package ru.potemkin.dating.domain.repository
 
+import androidx.lifecycle.LiveData
 import ru.potemkin.dating.domain.entities.*
 
 interface DatingRepository {
     suspend fun likeUser(
         currentUserId: String,
         userIdToLike: String
-    ): Result<Boolean>
+    ): LiveData<Boolean>
 
     suspend fun rejectUser(
         currentUserId: String,
         userIdToReject: String
-    ): Result<Boolean>
+    ): LiveData<Boolean>
 
     suspend fun getUsers(
         currentUserId: String,
         filters: UserFilters
-    ): Result<List<User>>
+    ): LiveData<List<User>>
 
     suspend fun getMatches(
         currentUserId: String
-    ): Result<List<User>>
+    ): LiveData<List<User>>
 
     suspend fun getUserProfile(
         currentUserId: String,
         profileUserId: String
-    ): Result<Profile>
+    ): LiveData<Profile>
 
     suspend fun updateUserProfile(
         currentUserId: String,
         updatedProfile: Profile
-    ): Result<Boolean>
+    ): LiveData<Boolean>
 
     suspend fun sendMessage(
         senderUserId: String,
         receiverUserId: String,
         message: String
-    ): Result<Boolean>
+    ): LiveData<Boolean>
 
     suspend fun getMessages(
         currentUserId: String,
         otherUserId: String
-    ): Result<List<Message>>
+    ): LiveData<List<Message>>
 
     suspend fun updateUserLocation(
         currentUserId: String,
         newLocation: Location
-    ): Result<Boolean>
+    ): LiveData<Boolean>
 
     suspend fun getUsersNearby(
         currentUserId: String,
         radius: Double
-    ): Result<List<User>>
+    ): LiveData<List<User>>
 }
