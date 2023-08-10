0b9efb162f75513fee31c43ab3990981c6d53431
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-10 14:55:31 +0300

    Создание всех функций репозитория в DatingRepositoryImpl.kt c TODO

--- app/src/main/java/ru/potemkin/dating/data/DatingRepositoryImpl.kt
+++ app/src/main/java/ru/potemkin/dating/data/DatingRepositoryImpl.kt
@@ -1,4 +1,78 @@
 package ru.potemkin.dating.data
 
-class DatingRepositoryImpl {
+import androidx.lifecycle.LiveData
+import ru.potemkin.dating.domain.entities.*
+import ru.potemkin.dating.domain.repository.DatingRepository
+
+class DatingRepositoryImpl(
+
+): DatingRepository {
+    override suspend fun likeUser(
+        currentUserId: String,
+        userIdToLike: String
+    ): LiveData<Boolean> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun rejectUser(
+        currentUserId: String,
+        userIdToReject: String
+    ): LiveData<Boolean> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getUsers(
+        currentUserId: String,
+        filters: UserFilters
+    ): LiveData<List<User>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getMatches(currentUserId: String): LiveData<List<User>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getUserProfile(
+        currentUserId: String,
+        profileUserId: String
+    ): LiveData<Profile> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateUserProfile(
+        currentUserId: String,
+        updatedProfile: Profile
+    ): LiveData<Boolean> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun sendMessage(
+        senderUserId: String,
+        receiverUserId: String,
+        message: String
+    ): LiveData<Boolean> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getMessages(
+        currentUserId: String,
+        otherUserId: String
+    ): LiveData<List<Message>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateUserLocation(
+        currentUserId: String,
+        newLocation: Location
+    ): LiveData<Boolean> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getUsersNearby(
+        currentUserId: String,
+        radius: Double
+    ): LiveData<List<User>> {
+        TODO("Not yet implemented")
+    }
+
 }
